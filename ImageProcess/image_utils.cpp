ae02b06d9579bc76bfec55beef15f64fc2b360ca
ReeWoo <reewoo4674@naver.com>
2022-06-13 22:20:15 +0900

    add Image process function

--- ImageProcess/image_utils.cpp
+++ ImageProcess/image_utils.cpp
@@ -36,3 +36,167 @@ void gamma_correction(const cv::Mat& img, const float k, cv::Mat& res)
 	res.convertTo(res, CV_8UC1, 255);
 	cv::imshow("gamma correction", res);
 }
+
+void draw_histogram(const cv::Mat& img, const std::string &str)
+{
+
+	cv::Mat histogram;
+
+	const int* channel_numbers = { 0 };
+	float channel_range[] = { 0.0, 255.0 };
+	const float* channel_ranges = channel_range;
+	int number_bins = 256;
+
+	calcHist(&img, 1, channel_numbers, cv::Mat(), histogram, 1, &number_bins, &channel_ranges);
+
+	std::cout << histogram.rows << " " << histogram.cols << std::endl; // 255, 1
+	std::cout << histogram.type() << std::endl;
+	std::cout << CV_32F << std::endl;
+	int width = 256 * 3;
+	int height = 700;
+	cv::Mat plane = cv::Mat::zeros(cv::Size(width, height), CV_8UC3);
+
+	double minVal;
+	double maxVal;
+	cv::Point minLoc;
+	cv::Point maxLoc;
+
+	cv::minMaxLoc(histogram, &minVal, &maxVal, &minLoc, &maxLoc);
+	std::cout << maxVal << " " << maxLoc <<std::endl;
+
+	float* hist = &histogram.at<float>(0);
+	int interval = 50;
+	for (int i = 0; i < number_bins; ++i)
+	{	
+		cv::line(plane, cv::Point(i * width / number_bins, height - (hist[i] / maxVal * (height - interval))), cv::Point(i * width / number_bins, height), cv::Scalar(128, 200, 128), 1);
+	}
+	cv::resize(plane, plane, cv::Size(img.cols, img.rows));
+	cv::imshow(str, plane);
+}
+
+void histogram_stretching(const cv::Mat& img, cv::Mat& res)
+{
+	double minVal;
+	double maxVal;
+	cv::Point minLoc;
+	cv::Point maxLoc;
+
+	cv::minMaxLoc(img, &minVal, &maxVal, &minLoc, &maxLoc);
+	res = (img - minVal) / (maxVal - minVal) * 255;
+
+	cv::imshow("histogram stretching", res);
+}
+
+void histogram_equalization(const cv::Mat& img, cv::Mat& res)
+{
+	cv::equalizeHist(img, res);
+	cv::imshow("histogram equalization", res);
+}
+
+void add_image(const cv::Mat& src1, const cv::Mat& src2, cv::Mat& res)
+{
+	res = src1 + src2;
+	cv::imshow("add image", res);
+}
+
+void sub_image(const cv::Mat& src1, const cv::Mat& src2, cv::Mat& res)
+{
+	res = src1 - src2;
+	cv::imshow("sub image", res);
+}
+
+void avg_image(const std::vector<cv::Mat>& src, cv::Mat& res)
+{
+	int n = src.size();
+	cv::Mat temp;
+
+	res = cv::Mat::zeros(src[0].size(), CV_8UC1);
+	for (int i = 0; i < n; ++i)
+	{
+		res += src[i] / n;
+	}
+	cv::imshow("avg image", res);
+}
+
+void abs_sub_image(cv::Mat& src1, cv::Mat& src2, cv::Mat& res)
+{
+	std::cout << "?" << std::endl;
+	if (src1.cols != src2.cols || src1.rows != src2.rows)
+		cv::resize(src2, src2, cv::Size(src1.cols, src2.rows));
+
+	cv::absdiff(src1, src2, res);
+	cv::imshow("abs sub image", res);
+}
+
+void and_image(const cv::Mat& src1, const cv::Mat& src2, cv::Mat& res)
+{
+	cv::bitwise_and(src1, src2, res);
+
+	cv::imshow("img2", src2);
+	/*res = cv::Mat::zeros(cv::Size(src1.rows, src1.cols), CV_8UC1);
+	for (int i = 0; i < src1.rows; ++i)
+	{
+		for (int j = 0; j < src1.cols; ++j)
+		{
+			res.data[i * res.cols + j] = src1.data[i * src1.cols + j] & src2.data[i * src2.cols + j];
+		}
+	}*/
+	cv::imshow("and image", res);
+}
+
+void or_image(const cv::Mat& src1, const cv::Mat& src2, cv::Mat& res)
+{	
+	//cv::bitwise_or(src1, src2, res);
+
+	cv::imshow("img2", src2);
+	res = cv::Mat::zeros(cv::Size(src1.rows, src1.cols), CV_8UC1);
+	for (int i = 0; i < src1.rows; ++i)
+	{
+		for (int j = 0; j < src1.cols; ++j)
+		{
+			res.data[i * res.cols + j] = src1.data[i * src1.cols + j] | src2.data[i * src2.cols + j];
+		}
+	}
+	cv::imshow("or image", res);
+
+}
+
+void bit_plane(const cv::Mat& src, cv::Mat& res, int bit)
+{
+	for (int i = 0; i < src.rows; ++i)
+	{
+		for (int j = 0; j < src.cols; ++j)
+		{
+			res.data[i * res.cols + j] = (src.data[i * src.cols + j] & (1 << bit)) ? 255 : 0;
+		}
+	}
+	cv::imshow("bit plane " + std::to_string(bit) + " image" , res);
+}
+
+void average_filter(const cv::Mat& src, cv::Mat& res, const unsigned int ksize)
+{
+	//cv::blur(src, res, cv::Size(ksize, ksize));
+	cv::Mat kernel = cv::Mat::ones(cv::Size(ksize, ksize), CV_32F);
+	kernel /= ksize * ksize;
+	cv::filter2D(src, res, -1, kernel);
+	std::string window_name = "평균값 필터 " + std::to_string(ksize);
+	cv::imshow(window_name, res);
+}
+
+void weighted_filter(const cv::Mat& src, cv::Mat& res)
+{
+	float data[9] = { 1, 2, 1, 2, 4, 2, 1, 2, 1 };
+	cv::Mat kernel = cv::Mat(3, 3, CV_32F, data);
+	kernel /= cv::sum(kernel);
+	cv::filter2D(src, res, -1, kernel);
+	std::string window_name = "가중 평균값 필터";
+	cv::imshow(window_name, res);
+}
+
+void gaussian(const cv::Mat& src, cv::Mat& res, const unsigned kernel_size, const double sigmaX, const double sigmaY)
+{
+	cv::GaussianBlur(src, res, cv::Size(kernel_size, kernel_size), sigmaX, sigmaY);
+	std::string window_name = "가우시안 필터 적용 " + std::to_string(kernel_size) + " " + std::to_string(sigmaX) + " " + std::to_string(sigmaY);
+	cv::imshow(window_name, res);
+	
+}
--- ImageProcess/main.cpp
+++ ImageProcess/main.cpp
@@ -1,17 +1,109 @@
 #include <stdio.h>
+#include <chrono>
 #include "image_utils.h"
 
 int main()
 {
 	printf("*** project start ***\n");
 	cv::Mat img, result;
+	//image_read("C:\\Users\\yoonw\\Downloads\\lena.jpg", img, cv::IMREAD_COLOR);
 	image_read("C:\\Users\\yoonw\\Downloads\\lena.jpg", img, cv::IMREAD_GRAYSCALE);
+	
 	brightness_inverse(img, result);
-	add_scalar(img, -100, result);
+	add_scalar(img, 150, result);
 	multiply_scalar(img, 1.5, result);
-	improve_contrast(img, 1.5, result);
+	draw_histogram(img, "original");
+	/*improve_contrast(img, 1.5, result);
+	draw_histogram(result);
 	gamma_correction(img, 0.6, result);
+	draw_histogram(result);*/
+	histogram_stretching(img, result);
+	draw_histogram(result, "hs");
+	histogram_equalization(img, result);
+	draw_histogram(result, "he");
 
+	cv::Mat img2 = cv::Mat::ones(cv::Size(img.cols, img.rows), CV_8UC1);
+	img2 = img2 * 255;
+	int width = img.cols / 2;
+	int height = img.rows / 2;
+	//cv::rectangle(img2, cv::Rect(width - width / 2, height - height / 2, width, height), cv::Scalar(0), -1);
+	cv::circle(img2, cv::Point(width, height), width / 2, cv::Scalar(0), -1);
+
+	cv::imshow("img2", img2);
+	add_image(img, img2, result);
+	//sub_image(img, img2, result);
+	sub_image(img2, img, result);
+
+	cv::Mat noise_image(img.size(), CV_8UC1);
+	double average = 0.0;
+	double std = 30.0;
+	randn(noise_image, cv::Scalar::all(average), cv:: Scalar::all(std));
+	
+	cv::Mat noise_image2(img.size(), CV_8UC1);
+	average = 5.0;
+	std = 20.0;
+	randn(noise_image2, cv::Scalar::all(average), cv::Scalar::all(std));
+
+	cv::Mat noise_image3(img.size(), CV_8UC1);
+	average = 10.0;
+	std = 25.0;
+	randn(noise_image3, cv::Scalar::all(average), cv::Scalar::all(std));
+
+	cv::Mat noise_image4(img.size(), CV_8UC1);
+	average = 15.0;
+	std = 15.0;
+	randn(noise_image4, cv::Scalar::all(average), cv::Scalar::all(std));
+
+	cv::Mat noise_image5(img.size(), CV_8UC1);
+	average = 20.0;
+	std = 10.0;
+	randn(noise_image5, cv::Scalar::all(average), cv::Scalar::all(std));
+	addWeighted(img, 1.0, noise_image, 1.0, 0.0, noise_image);
+	addWeighted(img, 1.0, noise_image2, 1.0, 0.0, noise_image2);
+	addWeighted(img, 1.0, noise_image3, 1.0, 0.0, noise_image3);
+	addWeighted(img, 1.0, noise_image4, 1.0, 0.0, noise_image4);
+	addWeighted(img, 1.0, noise_image5, 1.0, 0.0, noise_image5);
+	/*noise_image += img;
+	noise_image2 += img;*/
+	cv::imshow("noise_image", noise_image);
+	cv::imshow("noise_image2", noise_image2);
+	cv::imshow("noise_image3", noise_image3);
+	cv::imshow("noise_image4", noise_image4);
+	cv::imshow("noise_image5", noise_image5);
+
+	std::vector<cv::Mat> vec;
+	vec.push_back(noise_image);
+	vec.push_back(noise_image2);
+	vec.push_back(noise_image3);
+	vec.push_back(noise_image4);
+	vec.push_back(noise_image5);
+	avg_image(vec, result);
+
+	/*cv::Mat img1, img3;
+	image_read("C:\\Users\\yoonw\\Downloads\\img1.png", img1, cv::IMREAD_GRAYSCALE);
+	image_read("C:\\Users\\yoonw\\Downloads\\img2.png", img3, cv::IMREAD_GRAYSCALE);
+
+	cv::imshow("image1", img1);
+	cv::imshow("image2", img3);
+	abs_sub_image(img1, img3, result);*/
+	
+	img2 = cv::Mat::ones(cv::Size(img.rows, img.cols), CV_8UC1);
+	img2 *= 127;
+	and_image(img, img2, result);
+	or_image(img, img2, result);
+
+	for (int i = 0; i < 8; ++i)
+		bit_plane(img, result, i);
+
+	average_filter(img, result, 3);
+	average_filter(img, result, 5);
+	weighted_filter(img, result);
+	double sigma = 1;
+	gaussian(img, result, 2 * 4 * sigma + 1, sigma, sigma);
+	sigma = 4;
+	gaussian(img, result, 2 * 4 * sigma + 1, sigma, sigma);
+	
 	cv::waitKey(0);
+	
 	return 0;
 }
